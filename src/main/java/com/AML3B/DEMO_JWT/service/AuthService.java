7fedb72d31279e90ce941b454678037632798c89
madhusnigdha <your-email@example.com>
2026-04-16 15:08:58 +0530

    first commit

---
+++ src/main/java/com/AML3B/DEMO_JWT/service/AuthService.java
@@ -0,0 +1,28 @@
+package com.AML3B.DEMO_JWT.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.AML3B.DEMO_JWT.repository.UserRepository;
+import com.AML3B.DEMO_JWT.security.JwtUtil;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    UserRepository repo;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    public String login(String username, String password) {
+
+        var user = repo.findByUsername(username);
+
+        if(user != null && user.getPassword().equals(password)) {
+            return jwtUtil.generateToken(username);
+        }
+
+        return "Invalid Credentials";
+    }
+}
